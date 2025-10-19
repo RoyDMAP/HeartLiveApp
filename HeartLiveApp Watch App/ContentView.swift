d2f696eb76ebf55434749cab6fb42f0337aa6994
RoyDMAP <roy.dimapilis@yahoo.com>
2025-10-18 23:25:13 -0700

    Initial Commit

---
+++ HeartLiveApp Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HeartLiveApp Watch App
+//
+//  Created by Roy Dimapilis on 10/18/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HeartLiveApp Watch App/HeartLiveAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeartLiveAppApp.swift
+//  HeartLiveApp Watch App
+//
+//  Created by Roy Dimapilis on 10/18/25.
+//
+
+import SwiftUI
+
+@main
+struct HeartLiveApp_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
